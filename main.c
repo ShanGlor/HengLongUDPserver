98a6d3b1f5d194c7dd11f5db04082453fc9fb1a4
Stefan Helmert <stefan.helmert@gmx.net>
2014-01-07 00:16:49 +0100

    output-thread (no gpio); timeout

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <arpa/inet.h>
 #define __STDC_FORMAT_MACROS
@@ -24,6 +25,16 @@ typedef struct henglongservconf_t
     uint16_t port;
     uint64_t timeout_ms;
 } henglongservconf_t;
+
+typedef struct output_thread_t
+{
+    char* filename;
+    int frame;
+    uint64_t timeout_master_us, timeout_slave_us;
+    uint8_t client_selected;
+} output_thread_t;
+
+
 
 henglongservconf_t getservconfig(char* conffilename)
 {
@@ -54,6 +65,40 @@ henglongservconf_t getservconfig(char* conffilename)
 }
 
 
+void *output_thread_fcn(void * arg)
+{
+    printf("pthread output started\n");
+
+    //int fevdev;
+    output_thread_t* args;
+
+    args = (output_thread_t*) arg;
+    //fevdev = open(args->filename, O_RDONLY);
+
+
+    while (1)
+    {
+        if(get_us()>args->timeout_master_us){
+            args->frame = 0xfe400f2c; // timeout
+            printf("*** Master timeout!\n");
+        }
+        if(get_us()>args->timeout_slave_us){
+            args->frame = 0xfe400f2c; // timeout
+            printf("*** Slave timeout! -- Slave %d\n", args->client_selected);
+        }
+
+        usleep(200000);
+        printf("OUTPUT_THREAD -- FRAME: %#x\n", args->frame);
+
+
+    }
+
+    printf("Exiting output thread.\n");
+    //close(fevdev);
+
+    pthread_exit(0);
+}
+
 
 
 int main(int argc, char**argv)
@@ -66,13 +111,21 @@ int main(int argc, char**argv)
     uint64_t time_us_recv;
     int frame_recv;
     henglongservconf_t conf;
-    uint8_t clisel, clinbr;
+    uint8_t clisel = 0 , clinbr = 0, client_selected = 0;
     unsigned char servoff;
+    pthread_t outthread;
+    output_thread_t output_thread_args;
+
 
     if(2!=argc){
         printf("\nThis program is intented to be run on the raspberry pi in the heng long tank. \n\n USAGE: UDPserver server.config\n\n Copyright (C) 2014 Stefan Helmert <stefan.helmert@gmx.net>\n\n");
         return 0;
     }
+
+    memset(recvline, 0, 64*sizeof(unsigned char));
+
+    output_thread_args.client_selected = 0;
+    if (pthread_create(&outthread, NULL, output_thread_fcn , (void *) &output_thread_args)) printf("failed to create thread\n");
 
 
     conf = getservconfig(argv[1]);
@@ -107,10 +160,20 @@ int main(int argc, char**argv)
         clisel = recvline[15];
         servoff = recvline[16];
         recvline[n] = 0;
+
         printf("RECV FRAME from %s:%u -- FRM_NBR: %5d, CLK_ERR: %16" PRIi64 ", BYTES recv: %3d, REFL_FRM: %#x, CLINBR: %d, CLISEL: %d, SERVOFF: %d\n", inet_ntoa(cliaddr.sin_addr), cliaddr.sin_port, frame_nbr_recv, get_us() - time_us_recv, n, frame_recv, clinbr, clisel, servoff);
-        if(servoff && 0==clinbr){
-            printf("Server stoped on client request.\n");
-            return 0; // Server beenden
+        if(client_selected==clinbr){
+            output_thread_args.frame = frame_recv;
+            output_thread_args.timeout_slave_us = get_us() + (uint64_t)conf.timeout_ms*1000;
+        }
+        output_thread_args.client_selected = client_selected;
+        if(0==clinbr){
+            output_thread_args.timeout_master_us = get_us() + (uint64_t)conf.timeout_ms*1000;
+            client_selected = clisel;
+            if(servoff){
+                printf("Server stoped on client request.\n");
+                return 0; // Server beenden
+            }
         }
 
     }
